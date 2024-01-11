dd6a8898ef14e05bacf40c77cf243452210a1949
arad.yaron <arad.yaron@cyberark.com>
2024-01-11 15:39:53 +0200

    update default list value to use default_factory

--- octo_infra_aws_python/models/actions/network/create_security_group.py
+++ octo_infra_aws_python/models/actions/network/create_security_group.py
@@ -8,8 +8,8 @@ class CreateSecurityGroup(BaseModel):
     vpc_id: str = Field(description="VPC of the security group to use")
     description: Optional[str] = Field(description="Description of the group", default="")
     ingress: List[NetworkRule] = Field(description="Ingress rules for a new security group",
-                                       default=[])
+                                       default_factory=list)
     egress: List[NetworkRule] = Field(description="Egress rules for a new security group",
-                                      default=[])
+                                      default_factory=list)
     tags: Dict[str, str] = Field(description="Tags to be used for the VPC",
                                  default={})
--- octo_infra_aws_python/models/network_rule.py
+++ octo_infra_aws_python/models/network_rule.py
@@ -6,6 +6,6 @@ class NetworkRule(BaseModel):
     protocol: str = Field(default="tcp")  # AWS indicates -1 to be all protocols
     from_port: conint(ge=-1, le=65535) = Field()  # AWS indicates -1 to be all ports
     to_port: conint(ge=-1, le=65535) = Field()  # AWS indicates -1 to be all ports
-    allowed_cidr: List[str] = Field(default=[])
-    allowed_groups: List[str] = Field(default=[])
+    allowed_cidr: List[str] = Field(default_factory=list)
+    allowed_groups: List[str] = Field(default_factory=list)
     description: str = Field(default='Automated Rule')
