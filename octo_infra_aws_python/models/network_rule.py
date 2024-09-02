09364e57e6a42c334e79585183fb21089c49898c
ofir iluz <iluzofir@gmail.com>
2024-09-02 13:56:57 +0300

    Minor fix on annotated (#4)

--- octo_infra_aws_python/models/network_rule.py
+++ octo_infra_aws_python/models/network_rule.py
@@ -5,7 +5,7 @@ from typing_extensions import Annotated
 
 class NetworkRule(BaseModel):
     protocol: str = Field(default="tcp")  # AWS indicates -1 to be all protocols
-    from_port: Annotated[int, Field(ge=-1, le=65535)] = Field()  # AWS indicates -1 to be all ports
-    to_port: Annotated[int, Field(ge=-1, le=65535)] = Field()  # AWS indicates -1 to be all ports
+    from_port: Annotated[int, Field(ge=-1, le=65535)]  # AWS indicates -1 to be all ports
+    to_port: Annotated[int, Field(ge=-1, le=65535)]  # AWS indicates -1 to be all ports
     allowed_cidr: List[str] = Field(default_factory=list)
     allowed_groups: List[str] = Field(default_factory=list)
